ed58ace847f5c1154f63d20d763bd653777fad0c
Jake Gaylor <jgaylor@fsw.com>
2016-12-11 13:54:55 -0700

    use custom name for deploy resource

--- cookbooks/pathfinder/recipes/api.rb
+++ cookbooks/pathfinder/recipes/api.rb
@@ -1,4 +1,4 @@
-app_node node['pathfinder']['api']['name'] do |r|
+deploy_nodejs node['pathfinder']['api']['name'] do |r|
   version node['pathfinder']['api']['version']
   dns_name node['pathfinder']['api']['dns_name']
   tar_strip_components 2
