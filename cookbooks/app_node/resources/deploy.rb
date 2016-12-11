660af3940d6fcf094c4110c15a8fb7900c53aacd
Jake Gaylor <jgaylor@fsw.com>
2016-12-11 15:35:24 -0700

    debug and namespace

--- cookbooks/app_node/resources/deploy.rb
+++ cookbooks/app_node/resources/deploy.rb
@@ -1,8 +1,8 @@
 # allows this to not be called app_node_deploy
 resource_name :deploy_nodejs
 
-property :name, String, name_property: true
-property :version, String, required: true
+property :app_name, String, name_property: true
+property :app_version, String, required: true
 property :dns_name, String, default: "localhost"
 property :app_user, String, default: "ubuntu"
 property :app_port, Fixnum, default: 3000
@@ -19,17 +19,18 @@ property :tar_strip_components, Fixnum, default: 0
 # end
 
 action :create do
-  include_recipe "nodejs"
-  include_recipe 'jhg_nginx'
-
-  artifact_package_name = "#{name}-#{version}.tar.gz"
+  puts app_name
+  artifact_package_name = "#{app_name}-#{app_version}.tar.gz"
+  puts app_name
   artifact_path = "/tmp/#{artifact_package_name}"
-  target_dir = "/opt/#{name}"
+  puts app_name
+  target_dir = "/opt/#{app_name}"
+  puts app_name
 
   awscli_s3_file artifact_path do
     region s3_region
     bucket s3_bucket
-    key "#{name}/#{name}-#{version}.tar.gz"
+    key "#{app_name}/#{app_name}-#{app_version}.tar.gz"
     timeout 1200
     owner app_user
     group app_user
@@ -53,7 +54,10 @@ action :create do
     action :extract_local
   end
 
-  template "#{node['nginx']['dir']}/sites-available/#{name}" do
+  include_recipe "nodejs"
+  include_recipe 'jhg_nginx'
+
+  template "#{node['nginx']['dir']}/sites-available/#{app_name}" do
     source 'app.nginx.conf.erb'
     owner 'root'
     group 'root'
@@ -68,22 +72,22 @@ action :create do
     action   :enable
   end
 
-  nginx_site "#{name}" do
+  nginx_site "#{app_name}" do
     enable true
   end
 
-  template "/etc/init/#{name}.conf" do
+  template "/etc/init/#{app_name}.conf" do
     source 'app.upstart.conf.erb'
     owner 'root'
     group 'root'
     mode '0744'
     variables({
-      :name => node,
+      :app_name => node,
       :app_dir => target_dir
     })
   end
 
-  service "#{name}" do
+  service "#{app_name}" do
     action [:enable, :start]
   end
 end
--- cookbooks/pathfinder/recipes/api.rb
+++ cookbooks/pathfinder/recipes/api.rb
@@ -1,5 +1,5 @@
 deploy_nodejs node['pathfinder']['api']['name'] do |r|
-  version node['pathfinder']['api']['version']
+  app_version node['pathfinder']['api']['version']
   dns_name node['pathfinder']['api']['dns_name']
   tar_strip_components 2
 end
