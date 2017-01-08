379e65c9d0e70646cb6ec09075a13f8deebd6b95
Jake Gaylor <jgaylor@fsw.com>
2017-01-07 20:08:30 -0700

    bumb deployed version

--- cookbooks/trump-dicks/attributes/default.rb
+++ cookbooks/trump-dicks/attributes/default.rb
@@ -1,4 +1,4 @@
 default['trump-dicks']['name'] = 'trump-dicks'
-default['trump-dicks']['version'] = '1.0.1'
+default['trump-dicks']['version'] = '1.0.2'
 # this value is actually also being coded into terraform, they need to match
 default['trump-dicks']['dns_name'] = "trump-dicks.opslab.xyz"
